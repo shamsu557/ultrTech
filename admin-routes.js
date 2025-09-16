e4b25ab961139c5636021d2741324f2088d4a17b
shamsu557 <118239877+shamsu557@users.noreply.github.com>
2025-09-16 16:34:23 +0100

    Add files via upload

--- admin-routes.js
+++ admin-routes.js
@@ -27,9 +27,9 @@ router.use(
     store: sessionStore,
     cookie: {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production", // only true if HTTPS
+      secure: process.env.NODE_ENV === "production" ? true : false,
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      maxAge: 24 * 60 * 60 * 1000, // 24 hours
+      maxAge: 24 * 60 * 60 * 1000
     },
   })
 );
